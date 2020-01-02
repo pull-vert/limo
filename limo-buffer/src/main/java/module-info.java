e056c471c00c90ab380b2a36391354e46d2bbe60
pull-vert <frederic.montariol@gmail.com>
2020-01-03 00:17:41 +0100

    add NotNull and Nullable annotations, and create ByBu interface

---
+++ limo-buffer/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/*
+ * This is free and unencumbered software released into the public domain, following <https://unlicense.org>
+ */
+
+module limo.buffer {
+	requires limo.common;
+
+	exports io.limo.buffer;
+}
---
+++ limo-common/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/*
+ * This is free and unencumbered software released into the public domain, following <https://unlicense.org>
+ */
+
+module limo.common {
+	requires jsr305;
+
+	exports io.limo.common;
+}
