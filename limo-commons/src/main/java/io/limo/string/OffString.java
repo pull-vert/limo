ac4608cab82da0790fd36368b4da22d3d81fa925
pull-vert <frederic.montariol@gmail.com>
2020-04-07 15:57:00 +0200

    add javadoc for OffString

--- limo-commons/src/main/java/io/limo/string/OffString.java
+++ limo-commons/src/main/java/io/limo/string/OffString.java
@@ -13,7 +13,7 @@ import java.nio.charset.Charset;
 import java.util.Objects;
 
 /**
- * String stored in off-heap memory using {@link MemorySegment} in UTF-8, the most widely used encoding
+ * String stored in off-heap memory as {@link MemorySegment} with a current {@link Charset}
  * <p>
  * OffStrings are constant (immutable), their values cannot be changed after they are created.
  */
@@ -21,15 +21,31 @@ public interface OffString extends AutoCloseable {
 
     @NotNull MemorySegment toSegment(@NotNull Charset charset);
 
+    /**
+     * @return the current off-heap {@link MemorySegment} encoded with {@link Charset}
+     * @see #getCharset() to obtain current charset
+     */
+    @NotNull MemorySegment getSegment();
+
+    /**
+     * @return the current {@link Charset} used to encode String in off-heap memory
+     * @see #getSegment() to obtain current MemorySegment
+     */
     @NotNull Charset getCharset();
 
     /**
-     * Best effort to return a String from the {@link MemorySegment} (that may contains too much for one single String)
+     * Best effort to return a String from the {@link MemorySegment} (that may be too big for one single String)
      *
      * @return a String that contains all chars from this OffString
      */
     @Override
     @NotNull String toString();
+
+    /**
+     * Close the {@link MemorySegment}
+     */
+    @Override
+    void close();
 
     static @NotNull OffString of(@NotNull String string, @NotNull Charset charset) {
         return new StringOffString(Objects.requireNonNull(string), Objects.requireNonNull(charset));
