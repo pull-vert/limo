21d083ce9d926a3e71f33a974df9b622957cb15e
pull-vert <frederic.montariol@gmail.com>
2020-04-23 23:24:39 +0200

    Gradle 6.4 (#1)
    * upgrade to gradle 6.4 (rc2) that support natively module. Remove org.javamodularity.moduleplugin
    * add a module-info.java in test to ensure ServiceLoader is properly working.
    
    Solution came from https://github.com/gradle/gradle/issues/12890

---
+++ limo-core-jdk14/src/test/java/module-info.java
@@ -0,0 +1,11 @@
+/*
+ * This is free and unencumbered software released into the public domain, following <https://unlicense.org>
+ */
+
+@SuppressWarnings("module")
+open module limo.limo.core.jdk14.test {
+    requires org.junit.jupiter.api;
+    requires org.assertj.core;
+    requires limo.core;
+    requires limo.core.jdk14;
+}
